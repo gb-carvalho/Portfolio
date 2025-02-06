5f779a5e5c009f49db499cef0f0fbe43431fe300
Gabriel Vieira <vieiragabriel@id.uff.br>
2025-02-06 13:31:42 -0300

    first commit

---
+++ Portfolio/Pages/Portfolio.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class PortfolioModel : PageModel
+    {
+        private readonly ILogger<PortfolioModel> _logger;
+
+        public PortfolioModel(ILogger<PortfolioModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
