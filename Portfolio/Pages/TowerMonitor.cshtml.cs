2db05d106599e313d16572791b599688f0efadd7
Gabriel Vieira <vieiragabriel@id.uff.br>
2025-02-17 15:35:19 -0300

    Adding tower monitor page.

---
+++ Portfolio/Pages/TowerMonitor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class TowerMonitorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
